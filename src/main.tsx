d03ea6728ae25b54450255809d1ce62d18ae82b3
mabupro <mabupro787@gmail.com>
2023-12-16 19:37:49 +0900

    change/firebase-settings-folder

--- src/main.tsx
+++ src/main.tsx
@@ -3,32 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
-// firebase
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: String(import.meta.env.VITE_API_KEY),
-  authDomain: String(import.meta.env.VITE_AUTH_DOMAIN),
-  projectId: String(import.meta.env.VITE_PROJECT_ID),
-  storageBucket: String(import.meta.env.VITE_STORAGE_BUCKET),
-  messagingSenderId: String(import.meta.env.VITE_MESSAGING_SENDER_ID),
-  appId: String(import.meta.env.VITE_APP_ID),
-  measurementId: String(import.meta.env.VITE_FIREBASE_MEASUREMENT_ID)
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-// You can also set up Google Authentication provider
-const googleProvider = new GoogleAuthProvider();
-
-export { app, auth, db, storage, googleProvider };
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
