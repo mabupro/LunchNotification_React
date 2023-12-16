f9f3cf282d0a5150a032ab056ca2015e0ba58a62
mabupro <mabupro787@gmail.com>
2023-12-16 18:41:22 +0900

    change/lunchcard-chirldern from Menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,14 +1,12 @@
-import { LunchCard } from "./Card/LunchCard";
+import React from 'react';
+import { LunchCard } from './Card/LunchCard';
 
-export const Menu = () => {
-
+export const Menu: React.FC = () => {
     return (
         <>
-            <LunchCard />
-            <LunchCard />
-            <LunchCard />
+            <LunchCard name="Menu Item 1" price={10} isSoldOut={false} />
+            <LunchCard name="Menu Item 2" price={15} isSoldOut={true} />
+            <LunchCard name="Menu Item 3" price={12} isSoldOut={false} />
         </>
-    )
-}
-
-
+    );
+};
