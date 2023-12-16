3ce4318ae2c7a90efacf126d163e28be915e7a29
mabupro <mabupro787@gmail.com>
2023-12-16 20:23:01 +0900

    create/fetch-menu from firestore

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,28 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LunchCard } from './Card/LunchCard';
-import { db } from '../main';
-import { collection, getDocs } from "firebase/firestore";
+import { db } from '../../.firebase/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
+interface MenuData {
+    name: string;
+    filename: string;
+    price: number;
+    is_soldout: boolean;
+}
 
 export const Menu: React.FC = () => {
+    const [menuData, setMenuData] = useState<MenuData[]>([]);
 
     const fetchMenuList = async () => {
+        try {
+            const querySnapshot = await getDocs(collection(db, 'menu'));
+            const menuList: MenuData[] = [];
 
-        const querySnapshot = await getDocs(collection(db, "menu"));
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
-    }
+            querySnapshot.forEach((doc) => {
+                menuList.push(doc.data() as MenuData);
+            });
+
+            setMenuData(menuList);
+        } catch (error) {
+            console.error('Error fetching menu list:', error);
+        }
+    };
 
     useEffect(() => {
         fetchMenuList();
-    }, [])
+    }, []);
 
     return (
         <>
-            <LunchCard name="Menu Item 1" fileName='' price={10} isSoldOut={false} />
-            <LunchCard name="Menu Item 2" fileName='' price={15} isSoldOut={true} />
-            <LunchCard name="Menu Item 3" fileName='' price={12} isSoldOut={false} />
+            {menuData.map((menuItem) => (
+                <LunchCard
+                    key={menuItem.filename}
+                    name={menuItem.name}
+                    // fileName={menuItem.filename}
+                    price={menuItem.price}
+                    isSoldOut={menuItem.is_soldout}
+                />
+            ))}
         </>
     );
 };
