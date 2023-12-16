0d9f99c07fa1b765246f5c1a94eac2e3101ce358
mi3765 <chips1225@outlook.jp>
2023-12-16 14:54:32 +0900

    change nav items and items color

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Header } from '../components/Header';
 
 export const User = () => {
     return (
-        <div>User</div>
+        <div>
+            <Header />
+        </div>
     )
 }
