85452bd0dbeefc20716e414b40b595aea1cdc1d5
mi3765 <chips1225@outlook.jp>
2023-12-16 15:14:32 +0900

    add header Admin.tsx

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Header } from '../components/Header'
 
 export const Admin = () => {
     return (
-        <div>Admin</div>
+        <div>
+            <Header />
+        </div>
     )
 }
