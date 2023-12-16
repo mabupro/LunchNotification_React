18497aac76c909166615e2fd8fe8b1e8c5a87317
mabupro <mabupro787@gmail.com>
2023-12-16 18:36:10 +0900

    add/menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { LunchCard } from "./Card/LunchCard";
 
 export const Menu = () => {
+
     return (
-        <div>Menu</div>
+        <>
+            <LunchCard />
+            <LunchCard />
+            <LunchCard />
+        </>
     )
 }
+
+
