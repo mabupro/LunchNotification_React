084a786a5e41c960e28246221728909a2584c7bf
mabupro <mabupro787@gmail.com>
2023-12-16 21:49:42 +0900

    create/login and page router

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import { Menu } from '../components/Menu'
 
 export const Admin = () => {
     return (
         <div>
             <Header />
+            <Menu />
         </div>
     )
 }
