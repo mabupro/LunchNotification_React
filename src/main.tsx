edbbfe1469423734bd0f4f121a6f2b60a04aa652
mabupro <mabupro787@gmail.com>
2023-12-16 15:32:10 +0900

    add/fireabse-settings

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,29 @@ import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
 // firebase
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// You can also set up Google Authentication provider
+const googleProvider = new GoogleAuthProvider();
+
+export { app, auth, db, storage, googleProvider };
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
