ed49b11e7805950a4020a0d6d259c83f0bfd4fdf
mabupro <mabupro787@gmail.com>
2023-12-16 19:30:37 +0900

    create/fetch lunchmenu fix/vite-env.d.ts

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { LunchCard } from './Card/LunchCard';
+import { db } from '../main';
+import { collection, getDocs } from "firebase/firestore";
 
 export const Menu: React.FC = () => {
+
+    const fetchMenuList = async () => {
+
+        const querySnapshot = await getDocs(collection(db, "menu"));
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    }
+
+    useEffect(() => {
+        fetchMenuList();
+    }, [])
+
     return (
         <>
             <LunchCard name="Menu Item 1" fileName='' price={10} isSoldOut={false} />
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
     readonly VITE_STORAGE_BUCKET: string;
     readonly VITE_MESSAGING_SENDER_ID: string;
     readonly VITE_APP_ID: string;
+    readonly VITE_FIREBASE_MEASUREMENT_ID: string;
 }
 
 interface ImportMeta {
