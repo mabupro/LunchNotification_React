539bd82fbdc783d6aa514a34bd7cf87d3f960a17
mabupro <mabupro787@gmail.com>
2023-12-16 15:46:29 +0900

    fix/fireabase-types

--- src/main.tsx
+++ src/main.tsx
@@ -10,12 +10,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: String(import.meta.env.VITE_API_KEY),
+  authDomain: String(import.meta.env.VITE_AUTH_DOMAIN),
+  projectId: String(import.meta.env.VITE_PROJECT_ID),
+  storageBucket: String(import.meta.env.VITE_STORAGE_BUCKET),
+  messagingSenderId: String(import.meta.env.VITE_MESSAGING_SENDER_ID),
+  appId: String(import.meta.env.VITE_APP_ID),
 };
 
 const app = initializeApp(firebaseConfig);
